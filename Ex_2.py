1c48cd80e789961c8b9e8d283227478fdfa97baf
Amanikashema <amanikashema000@gmail.com>
2020-03-09 16:12:53 +0200

    first commit

---
+++ Ex_2.py
@@ -0,0 +1,10 @@
+
+import math #including import maths to use mathematical functions
+a=8
+b=2
+c=1
+d=4
+
+x =  (math.sqrt(a-3))/(b^2+c^2+d^2) #mathematical expression
+
+print(x) #this is the answer of X
